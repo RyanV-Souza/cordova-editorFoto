0f832e098c40e7fd665a840277a34577f63d9055
RyanV-Souza <ryanvsouza99@gmail.com>
2019-09-13 03:31:12 +0900

    Criando a função de chamar a câmera, criando as inputs de alteração de valores

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,34 @@
 // This is a JavaScript file
+$(document).ready(function(){
 
+  //Div's escondidas no inicio
+  
+
+  $(document).on("click", "#chamarCamera", function(){
+  
+  navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI });
+
+  function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+  }
+
+  function onFail(message) {
+    alert('Failed because: ' + message);
+  }
+
+  function setOptions(srcType) {
+    var options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: srcType,
+        allowEdit: true,
+        correctOrientation: true  //Corrects Android orientation quirks
+    }
+    return options;
+  }
+
+})
+
+});
